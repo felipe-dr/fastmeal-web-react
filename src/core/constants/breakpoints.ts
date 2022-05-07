2a6155b17c39cce47678bbcc5a41060a467ff5f0
Felipe DR <felipe.corp7@gmail.com>
2022-05-07 11:44:14 -0300

    feat(core): add breakpoint and route constants

---
+++ src/core/constants/breakpoints.ts
@@ -0,0 +1,9 @@
+const BREAKPOINTS = {
+  xs: '(max-width: 319px)',
+  sm: '(max-width: 599px)',
+  md: '(max-width: 899px)',
+  lg: '(max-width: 1199px)',
+  xl: '(max-width: 1449px)',
+};
+
+export default Object.freeze(BREAKPOINTS);
