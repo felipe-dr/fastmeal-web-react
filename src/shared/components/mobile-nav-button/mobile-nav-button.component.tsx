5d4289572933e1cf9a51c3b46c46469bfb205494
Felipe DR <felipe.corp7@gmail.com>
2022-05-07 11:46:32 -0300

    feat(shared): implements MobileNavButton

---
+++ src/shared/components/mobile-nav-button/mobile-nav-button.component.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+
+import { useNavbarContext } from 'core/contexts/navbar/navbar.context';
+
+import styles from './mobile-nav-button.module.scss';
+
+export default function MobileNavButton(): JSX.Element {
+  const { showNavbar, handleNavbar } = useNavbarContext();
+
+  return (
+    <button
+      className={classNames({
+        [styles['c-mobile-nav-button']]: true,
+        [styles['c-mobile-nav-button--active']]: showNavbar,
+      })}
+      type="button"
+      onClick={handleNavbar}
+    >
+      {' '}
+    </button>
+  );
+}
