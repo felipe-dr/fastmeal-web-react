906908a2a60efe06d02a1c5239dcf841a8171df7
Felipe DR <felipe.corp7@gmail.com>
2022-05-15 16:23:54 -0300

    styles(layouts): add typography class to footer and remove fixed value in l-footer

--- src/layouts/website/footer/footer.component.tsx
+++ src/layouts/website/footer/footer.component.tsx
@@ -4,7 +4,7 @@ export default function Footer(): JSX.Element {
   return (
     <footer className={styles['l-footer']}>
       <div className="grid-container u-text-center">
-        <p>
+        <p className="s2">
           <span className="u-font-bold">Fastmeal</span> © 2022 - Alguns direitos
           reservados.
         </p>
