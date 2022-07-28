6789911b90a17b2cd3c348a5ea45c7743a05fd10
Felipe DR <felipe.corp7@gmail.com>
2022-07-28 19:53:11 -0300

    feat(features): add form submission method

--- src/features/auth/components/sign-up/sign-up.component.tsx
+++ src/features/auth/components/sign-up/sign-up.component.tsx
@@ -1,32 +1,41 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { FormEvent, useEffect, useState } from 'react';
 import { RiLock2Line, RiMailLine, RiUserLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
+import useFetch from 'core/hooks/use-fetch/use-fetch.hook';
 import useForm, {
-  handleClear,
+  handleResponse,
   handleSubmit,
-} from 'core/hooks/use-form/useForm.hook';
+} from 'core/hooks/use-form/use-form.hook';
+import AuthService from 'core/services/auth/auth.service';
 
 import ButtonLink from 'shared/components/button-link/button-link.component';
+import Errors from 'shared/components/errors/errors.component';
 import Input from 'shared/components/input/input.component';
+import { ServiceRequest } from 'shared/types/service-request.type';
+
+import { User } from 'features/auth/interfaces/user.interface';
 
 export default function SignUp(): JSX.Element {
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+  const { request, statusMessage, errors } = useFetch<User>();
 
   const formFields = {
-    userName: useForm({
+    name: useForm({
       validators: [{ required: true }, { minlength: 3 }],
       fieldName: 'Nome',
     }),
-    userEmail: useForm({
+    email: useForm({
       validators: [{ required: true }, { email: true }],
       fieldName: 'E-mail',
     }),
-    userPassword: useForm({
+    password: useForm({
       validators: [{ required: true }, { minlength: 4 }],
       fieldName: 'Senha',
     }),
-    userPasswordConfirm: useForm({
+    passwordConfirm: useForm({
       validators: [{ required: true }, { minlength: 4 }, { equals: password }],
       fieldName: 'Confirmar senha',
       customMessage: 'Senhas não conferem',
@@ -34,29 +43,51 @@ export default function SignUp(): JSX.Element {
   };
 
   useEffect(() => {
-    setPassword(formFields.userPassword.value);
-  }, [formFields.userPassword.value]);
+    setPassword(formFields.password.value);
+  }, [formFields.password.value]);
 
-  function onSubmit(event: FormEvent<HTMLFormElement>): void {
-    if (handleSubmit({ event, formFields })) {
-      alert('Registro efetuado com sucesso');
+  async function onSubmit(
+    event: FormEvent<HTMLFormElement>,
+    serviceRequest: ServiceRequest<User>
+  ): Promise<void> {
+    const { isValidForm, formFieldsObject } = handleSubmit({ formFields });
+    let isSuccessResponse: boolean;
 
-      handleClear({ formFields });
+    event.preventDefault();
+
+    if (isValidForm) {
+      const authService = AuthService.getInstance();
+      const response = await serviceRequest(
+        authService.create(formFieldsObject)
+      );
+
+      isSuccessResponse = handleResponse({ response, formFields });
+
+      if (isSuccessResponse) {
+        navigate(-2);
+      }
     }
   }
 
   return (
     <>
+      {errors?.length > 0 && (
+        <Errors
+          styleClasses="u-mt-15"
+          errorTitle={statusMessage}
+          errors={errors}
+        />
+      )}
       <form
-        className="u-display-grid u-gap-y-20 u-width-100 u-mt-30 u-mb-45"
-        onSubmit={onSubmit}
+        className="l-form--auth u-display-grid u-gap-y-20 u-width-100 u-mt-30 u-mb-45"
+        onSubmit={(event) => onSubmit(event, request)}
       >
         <Input
           name="name"
           type="text"
           placeholder="Nome"
           autoFocus
-          {...formFields.userName}
+          {...formFields.name}
         >
           <RiUserLine size={20} className="u-text-base-3" />
         </Input>
@@ -64,7 +95,7 @@ export default function SignUp(): JSX.Element {
           name="email"
           type="text"
           placeholder="E-mail"
-          {...formFields.userEmail}
+          {...formFields.email}
         >
           <RiMailLine size={20} className="u-text-base-3" />
         </Input>
@@ -72,7 +103,7 @@ export default function SignUp(): JSX.Element {
           name="password"
           type="password"
           placeholder="Senha"
-          {...formFields.userPassword}
+          {...formFields.password}
         >
           <RiLock2Line size={20} className="u-text-base-3" />
         </Input>
@@ -80,7 +111,7 @@ export default function SignUp(): JSX.Element {
           name="password-confirm"
           type="password"
           placeholder="Confirmar senha"
-          {...formFields.userPasswordConfirm}
+          {...formFields.passwordConfirm}
         >
           <RiLock2Line size={20} className="u-text-base-3" />
         </Input>
