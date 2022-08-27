5538613c17478b27ff4ac28c14541e7bbc61d11d
Felipe DR <felipe.corp7@gmail.com>
2022-08-27 14:37:22 -0300

    feat(shared): adds hoverColor, mobileLabel and styleClasses properties as well as rounded button appearance

--- src/shared/components/button-link/button-link.component.tsx
+++ src/shared/components/button-link/button-link.component.tsx
@@ -1,74 +1,106 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/destructuring-assignment */
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
-import {
-  ButtonLinkAppearance,
-  ButtonLinkVisualType,
-} from 'shared/types/button-link.type';
 import { Colors } from 'shared/types/colors.type';
 
 import styles from './button-link.module.scss';
+import { ButtonLinkAppearance } from './types/button-link-appearance.type';
+import { ButtonLinkVisualType } from './types/button-link-visual-type.type';
 import { ButtonLinkProps } from './types/button-link.type';
 
 export default function ButtonLink(props: ButtonLinkProps): JSX.Element {
   function addStyleClasses(
     visualType: ButtonLinkVisualType | undefined,
     appearance: ButtonLinkAppearance | undefined,
-    color: Colors | undefined
+    color: Colors | undefined,
+    hoverColor: Colors | undefined
   ): string {
     return classNames({
       [styles[`c-${visualType}`]]: true,
       [styles[`c-${visualType}--${appearance}`]]: appearance !== undefined,
       [styles[`c-${visualType}--color-${color}`]]: color !== undefined,
+      [styles[`c-${visualType}--hover-color-${hoverColor}`]]:
+        hoverColor !== undefined,
       [`${props.styleClasses}`]: props.styleClasses !== undefined,
     });
   }
 
   if (props.elementType === 'button') {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { elementType, visualType, appearance, color, ...rest } = props;
+    const {
+      elementType,
+      visualType,
+      appearance,
+      color,
+      hoverColor,
+      mobileLabel,
+      styleClasses,
+      ...rest
+    } = props;
 
     return (
-      // eslint-disable-next-line react/button-has-type
       <button
-        className={addStyleClasses(visualType, appearance, color)}
-        // eslint-disable-next-line react/jsx-props-no-spreading
+        className={addStyleClasses(visualType, appearance, color, hoverColor)}
         {...rest}
       >
         {props.children}
+        {props.mobileLabel && (
+          <span className={styles['c-button__label']}>{props.mobileLabel}</span>
+        )}
       </button>
     );
   }
 
   if (props.elementType === 'link') {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { elementType, visualType, appearance, color, ...rest } = props;
+    const {
+      elementType,
+      visualType,
+      appearance,
+      color,
+      hoverColor,
+      mobileLabel,
+      styleClasses,
+      ...rest
+    } = props;
 
     return (
       <Link
-        className={addStyleClasses(visualType, appearance, color)}
-        // eslint-disable-next-line react/jsx-props-no-spreading
+        className={addStyleClasses(visualType, appearance, color, hoverColor)}
         {...rest}
       >
         {props.children}
+        {props.mobileLabel && (
+          <span className={styles['c-button__label']}>{props.mobileLabel}</span>
+        )}
       </Link>
     );
   }
 
   if (props.elementType === 'externalLink') {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { elementType, visualType, appearance, color, ...rest } = props;
+    const {
+      elementType,
+      visualType,
+      appearance,
+      color,
+      hoverColor,
+      mobileLabel,
+      styleClasses,
+      ...rest
+    } = props;
 
     return (
       <a
-        className={addStyleClasses(visualType, appearance, color)}
+        className={addStyleClasses(visualType, appearance, color, hoverColor)}
         target="_blank"
         rel="noopener noreferrer"
-        // eslint-disable-next-line react/jsx-props-no-spreading
         {...rest}
       >
         {props.children}
+        {props.mobileLabel && (
+          <span className={styles['c-button__label']}>{props.mobileLabel}</span>
+        )}
       </a>
     );
   }
