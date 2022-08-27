6d9883e92ddf066b88dbe0114a8d0ee55b125489
Felipe DR <felipe.corp7@gmail.com>
2022-08-27 14:41:20 -0300

    refactor(shared): add new color option

--- src/shared/types/colors.type.ts
+++ src/shared/types/colors.type.ts
@@ -1,1 +1,7 @@
-export type Colors = 'primary' | 'secondary' | 'base-1' | 'base-2' | 'base-3';
+export type Colors =
+  | 'primary'
+  | 'secondary'
+  | 'base-1'
+  | 'base-2'
+  | 'base-3'
+  | 'transparent';
