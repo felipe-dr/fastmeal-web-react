9479b03306b5141272c01b6042bbac1ae6423961
Felipe DR <felipe.corp7@gmail.com>
2022-09-19 19:06:35 -0300

    refactor(core): remove unused file

--- src/core/utils/conversions/conversions.util.ts
+++
@@ -1,8 +0,0 @@
-export default function stringToBoolean(value: string) {
-  return value === 'false' ||
-    value === 'undefined' ||
-    value === 'null' ||
-    value === '0'
-    ? false
-    : !!value;
-}
