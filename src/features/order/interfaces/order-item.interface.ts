2bbcb054c7a205e361dfcc319e1ddfaa68a7d764
Felipe DR <felipe.corp7@gmail.com>
2022-10-13 18:50:12 -0300

    feat(features): add mock orderItems temporarily

--- src/features/order/interfaces/order-item.interface.ts
+++ src/features/order/interfaces/order-item.interface.ts
@@ -1,7 +1,30 @@
+const temp = '/images/foods/burger.png';
+
 export interface OrderItem {
   id: number;
   name: string;
+  description: string;
   quantity: number;
   price: number;
   imagePath: string;
 }
+
+// !! TEMP
+export const orderItems: OrderItem[] = [
+  {
+    id: 1,
+    name: 'Pão artesanal italiano',
+    description: 'Pão artesanal com queijos italianos',
+    quantity: 1,
+    price: 15.9,
+    imagePath: temp,
+  },
+  {
+    id: 2,
+    name: 'Donut',
+    description: 'Coberto com chantilly',
+    quantity: 4,
+    price: 2.5,
+    imagePath: temp,
+  },
+];
