6a4a394584bc8b4475040f4f56bc1439705d69e8
Felipe DR <felipe.corp7@gmail.com>
2022-10-13 18:46:56 -0300

    refactor(features): remove orderItems mock and update styles

--- src/features/order/components/order-dropdown/order-dropdown.component.tsx
+++ src/features/order/components/order-dropdown/order-dropdown.component.tsx
@@ -1,35 +1,17 @@
 import classNames from 'classnames';
-import { RiShoppingBasketLine } from 'react-icons/ri';
+import { RiBankCard2Line, RiShoppingBasketLine } from 'react-icons/ri';
 
 import toBRL from 'core/utils/conversions/currency.util';
 
 import ButtonLinkComponent from 'shared/components/button-link/button-link.component';
 import DropdownComponent from 'shared/components/dropdown/dropdown.component';
 
-import { OrderItem } from 'features/order/interfaces/order-item.interface';
+import { orderItems } from 'features/order/interfaces/order-item.interface';
 
 import OrderItemDropdownComponent from '../order-item-dropdown/order-item-dropdown.component';
 import orderDropdownStyles from './order-dropdown.module.scss';
 
 export default function OrderDropdownComponent(): JSX.Element {
-  // !! TEMP
-  const orderItems: OrderItem[] = [
-    {
-      id: 1,
-      name: 'Pão Artesanal Italiano',
-      quantity: 1,
-      price: 15.9,
-      imagePath: '',
-    },
-    {
-      id: 2,
-      name: 'Classic Burger',
-      quantity: 2,
-      price: 18.5,
-      imagePath: '',
-    },
-  ];
-
   return (
     <DropdownComponent
       buttonAppearance="round"
@@ -61,23 +43,27 @@ export default function OrderDropdownComponent(): JSX.Element {
           >
             <tbody>
               <tr>
-                <th className="u-font-bold">Subtotal:</th>
-                <td className="u-text-right">{toBRL(99.8)}</td>
+                <th className="u-font-bold u-pl-20" scope="row">
+                  Subtotal:
+                </th>
+                <td className="u-text-right u-pr-20">{toBRL(25.9)}</td>
               </tr>
             </tbody>
           </table>
           <ButtonLinkComponent
             elementType="link"
             visualType="button"
-            appearance="default"
+            appearance="compact"
             color="base-3"
             hoverColor="primary"
             to="order/checkout"
             styleClasses={classNames({
               [orderDropdownStyles['c-order-dropdown__button']]: true,
-              'u-display-block': true,
+              'u-display-flex u-flex-justify-content-center u-flex-align-items-center':
+                true,
             })}
           >
+            <RiBankCard2Line className="u-flex-shrink-0 u-mr-5" />
             Fechar pedido
           </ButtonLinkComponent>
         </>
