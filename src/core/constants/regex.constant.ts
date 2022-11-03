ed7a3a271a52796f331102a051aaa16f7762ffe5
Felipe DR <felipe.corp7@gmail.com>
2022-11-03 19:49:59 -0300

    feat(core): add new validators

--- src/core/constants/regex.constant.ts
+++ src/core/constants/regex.constant.ts
@@ -1,6 +1,10 @@
-export const formRegex = {
+const formValidationsRegex = Object.freeze({
+  ONLY_LETTERS: /^([A-zÀ-ÖØ-öø-ÿ]+\s)*[A-zÀ-ÖØ-öø-ÿ]+$/,
+  ONLY_NUMBERS: /^[0-9]+$/,
+  PHONE:
+    /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/,
   EMAIL:
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-};
+});
 
-export default formRegex;
+export default formValidationsRegex;
