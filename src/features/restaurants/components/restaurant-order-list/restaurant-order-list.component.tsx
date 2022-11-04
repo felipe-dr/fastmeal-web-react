ea7c5f140b545f94772f12320fa0bb0064c1cb6a
Felipe DR <felipe.corp7@gmail.com>
2022-11-04 18:31:14 -0300

    refactor(features): change content display logic

--- src/features/restaurants/components/restaurant-order-list/restaurant-order-list.component.tsx
+++ src/features/restaurants/components/restaurant-order-list/restaurant-order-list.component.tsx
@@ -28,28 +28,28 @@ export default function RestaurantOrderListComponent(): JSX.Element {
           [restaurantOrderListStyles['c-restaurant-order-list__title']]: true,
           't3 u-text-base-3 u-text-uppercase': true,
           'u-display-flex u-flex-align-items-center': true,
-          'u-p-20': true,
+          'u-px-20 u-pb-20': true,
         })}
       >
         <RiShoppingBasketLine className="u-flex-shrink-0 u-mr-5" />
         Pedido
       </h4>
-      {orderItems.length > 0 ? (
+      <div
+        className={classNames({
+          [orderListStyles['c-order-list__container']]: orderItems.length > 0,
+          [orderListStyles['c-order-list__container--max-height-small']]:
+            orderItems.length > 0,
+          'u-px-20 u-mt-20': true,
+        })}
+      >
+        <OrderListComponent
+          showImage={false}
+          showDescription={false}
+          showPrice={false}
+        />
+      </div>
+      {orderItems.length > 0 && (
         <>
-          <div
-            className={classNames({
-              [orderListStyles['c-order-list__container']]: true,
-              [orderListStyles['c-order-list__container--max-height-small']]:
-                true,
-              'u-px-20 u-mt-20': true,
-            })}
-          >
-            <OrderListComponent
-              showImage={false}
-              showDescription={false}
-              showPrice={false}
-            />
-          </div>
           <table
             className={
               restaurantOrderListStyles['c-restaurant-order-list__subtotal']
@@ -64,43 +64,40 @@ export default function RestaurantOrderListComponent(): JSX.Element {
               </tr>
             </tbody>
           </table>
+          <div
+            className={classNames({
+              [restaurantOrderListStyles['c-restaurant-order-list__actions']]:
+                true,
+              'u-flex-justify-content-center u-flex-sm-justify-content-end':
+                true,
+            })}
+          >
+            <ButtonLinkComponent
+              elementType="button"
+              visualType="button"
+              appearance="compact"
+              color="base-1"
+              hoverColor="primary"
+              styleClasses="u-display-flex u-flex-align-items-center"
+              type="button"
+            >
+              <RiCloseCircleLine className="u-flex-shrink-0 u-mr-5" />
+              Limpar
+            </ButtonLinkComponent>
+            <ButtonLinkComponent
+              elementType="link"
+              visualType="button"
+              appearance="compact"
+              color="base-3"
+              hoverColor="primary"
+              to="/order/checkout"
+              styleClasses="u-display-flex u-flex-align-items-center"
+            >
+              <RiBankCard2Line className="u-flex-shrink-0 u-mr-5" />
+              Fechar pedido
+            </ButtonLinkComponent>
+          </div>
         </>
-      ) : (
-        <p className="u-p-20">Nenhum item adicionado.</p>
-      )}
-      {orderItems.length > 0 && (
-        <div
-          className={classNames({
-            [restaurantOrderListStyles['c-restaurant-order-list__actions']]:
-              true,
-            'u-flex-justify-content-center u-flex-sm-justify-content-end': true,
-          })}
-        >
-          <ButtonLinkComponent
-            elementType="button"
-            visualType="button"
-            appearance="compact"
-            color="base-1"
-            hoverColor="primary"
-            styleClasses="u-display-flex u-flex-align-items-center"
-            type="button"
-          >
-            <RiCloseCircleLine className="u-flex-shrink-0 u-mr-5" />
-            Limpar
-          </ButtonLinkComponent>
-          <ButtonLinkComponent
-            elementType="link"
-            visualType="button"
-            appearance="compact"
-            color="base-3"
-            hoverColor="primary"
-            to="/order/checkout"
-            styleClasses="u-display-flex u-flex-align-items-center"
-          >
-            <RiBankCard2Line className="u-flex-shrink-0 u-mr-5" />
-            Fechar pedido
-          </ButtonLinkComponent>
-        </div>
       )}
     </div>
   );
