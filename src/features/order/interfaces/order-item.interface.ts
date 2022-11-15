fd3ab67b963a4cc924a8774c5aaaa121ee92c669
Felipe DR <felipe.corp7@gmail.com>
2022-11-15 18:20:20 -0300

    refactor(features): remove mocked orderItems

--- src/features/order/interfaces/order-item.interface.ts
+++ src/features/order/interfaces/order-item.interface.ts
@@ -1,7 +1,5 @@
-const temp = '/images/foods/burger.png';
-
 export interface OrderItem {
-  id: number;
+  id: string;
   name: string;
   description: string;
   quantity: number;
@@ -9,34 +7,3 @@ export interface OrderItem {
   subtotal: number;
   imagePath: string;
 }
-
-// !! TEMP
-export const orderItems: OrderItem[] = [
-  {
-    id: 1,
-    name: 'Pão artesanal italiano',
-    description: 'Pão artesanal com queijos italianos',
-    quantity: 1,
-    price: 15.9,
-    subtotal: 15.9,
-    imagePath: temp,
-  },
-  {
-    id: 2,
-    name: 'Donut',
-    description: 'Coberto com chantilly',
-    quantity: 4,
-    price: 2.5,
-    subtotal: 10.2,
-    imagePath: temp,
-  },
-  {
-    id: 3,
-    name: 'Donut',
-    description: 'Coberto com chantilly',
-    quantity: 4,
-    price: 2.5,
-    subtotal: 10.2,
-    imagePath: temp,
-  },
-];
