e28d5446e7d06216e5cf92ed8de6de4b135d7393
Felipe DR <felipe.corp7@gmail.com>
2022-11-28 19:44:56 -0300

    add call to SpinnerComponent

--- src/features/restaurants/components/restaurant-menu/restaurant-menu.component.tsx
+++ src/features/restaurants/components/restaurant-menu/restaurant-menu.component.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import classNames from 'classnames';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import useFetch from 'core/hooks/use-fetch/use-fetch.hook';
 
 import ErrorsComponent from 'shared/components/errors/errors.component';
+import SpinnerComponent from 'shared/components/spinner/spinner.component';
 
 import { RestaurantMenuItem } from 'features/restaurants/interfaces/restaurant-menu-item.interface';
 import RestaurantsService from 'features/restaurants/services/restaurants/restaurants.service';
@@ -26,7 +28,7 @@ export default function RestaurantMenuComponent(): JSX.Element {
   }, []);
 
   if (isLoading) {
-    return <p>Carregando...</p>;
+    return <SpinnerComponent />;
   }
 
   if (errors.length > 0) {
@@ -36,7 +38,16 @@ export default function RestaurantMenuComponent(): JSX.Element {
   return (
     <div className={styles['l-restaurant-menu']}>
       {data?.length === 0 ? (
-        <p>Não há itens do cardápio disponíveis no momento.</p>
+        <p
+          className={classNames({
+            [styles['l-restaurant-menu__text']]: true,
+            'u-text-center': true,
+            'u-text-sm-left': true,
+            'u-shadow-small': true,
+          })}
+        >
+          Não há itens do cardápio disponíveis no momento.
+        </p>
       ) : (
         data?.map((restaurantMenuItem) => (
           <RestaurantMenuItemComponent
--- src/features/restaurants/pages/restaurant-details/restaurant-details.component.tsx
+++ src/features/restaurants/pages/restaurant-details/restaurant-details.component.tsx
@@ -6,6 +6,7 @@ import useFetch from 'core/hooks/use-fetch/use-fetch.hook';
 
 import ErrorsComponent from 'shared/components/errors/errors.component';
 import HeaderTitleComponent from 'shared/components/header-title/header-title.component';
+import SpinnerComponent from 'shared/components/spinner/spinner.component';
 
 import RestaurantAboutComponent from 'features/restaurants/components/restaurant-about/restaurant-about.component';
 import { Restaurant } from 'features/restaurants/interfaces/restaurant.interface';
@@ -25,7 +26,12 @@ export default function RestaurantDetailsComponent(): JSX.Element {
   }, []);
 
   if (isLoading) {
-    return <p>Carregando...</p>;
+    return (
+      <>
+        <HeaderTitleComponent titleText="Restaurantes" />
+        <SpinnerComponent />;
+      </>
+    );
   }
 
   if (errors.length > 0) {
@@ -39,10 +45,12 @@ export default function RestaurantDetailsComponent(): JSX.Element {
   return (
     <section>
       <HeaderTitleComponent titleText={data?.name as string} />
-      <div className="grid-container u-display-grid u-gap-30">
-        <RestaurantAboutComponent {...data} />
-        <Outlet />
-      </div>
+      {data && (
+        <div className="grid-container u-display-grid u-gap-30">
+          <RestaurantAboutComponent {...data} />
+          <Outlet />
+        </div>
+      )}
     </section>
   );
 }
