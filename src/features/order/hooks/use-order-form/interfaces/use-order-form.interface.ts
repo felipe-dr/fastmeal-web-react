34705ac48fee56e0f0c6dbfe7b4b6ddde51fa5c5
Felipe DR <felipe.corp7@gmail.com>
2023-04-07 16:20:58 -0300

    feat(features): implement form submission and remove account fields

--- src/features/order/hooks/use-order-form/interfaces/use-order-form.interface.ts
+++ src/features/order/hooks/use-order-form/interfaces/use-order-form.interface.ts
@@ -2,5 +2,7 @@ import { OrderPaymentFormProps } from 'features/order/interfaces/order-payment-f
 
 export interface UseOrderFormReturn extends OrderPaymentFormProps {
   formFields: { [key: string]: object };
+  hasOrderItems: boolean;
+  isLoading: boolean;
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }
--- src/features/order/hooks/use-order-form/use-order-form.hook.ts
+++ src/features/order/hooks/use-order-form/use-order-form.hook.ts
@@ -3,20 +3,34 @@ import { useNavigate } from 'react-router-dom';
 
 import { useOrderContext } from 'core/contexts/order/order.context';
 import { useUserContext } from 'core/contexts/user/user.context';
+import useFetch from 'core/hooks/use-fetch/use-fetch.hook';
 import useFormInput from 'core/hooks/use-form-input/use-form-input.hook';
 import useFormRadio from 'core/hooks/use-form-radio/use-form-radio.hook';
 import useForm from 'core/hooks/use-form/use-form.hook';
+import {
+  sanitizeField,
+  sanitizePhone,
+} from 'core/utils/forms/sanitation/sanitation.util';
 
+import { OrderCheckout } from 'features/order/interfaces/order-checkout.interface';
+import OrderCheckoutService from 'features/order/services/order-checkout/order-checkout.service';
+
+import {
+  FormDataGroupingParams,
+  FormDataGroupingReturn,
+} from './interfaces/form-data-grouping.interface';
+import { SanitizesWithSpecificityParams } from './interfaces/sanitizes-with-specificity.interface';
 import { UseOrderFormReturn } from './interfaces/use-order-form.interface';
 
 export default function useOrderForm(): UseOrderFormReturn {
-  const { handleSubmit } = useForm();
-  const { orderItems } = useOrderContext();
+  const { handleSubmit, handleResponse } = useForm();
+  const { orderItems, clearOrderItems } = useOrderContext();
   const { isAuthenticatedUser } = useUserContext();
-  const [password, setPassword] = useState<string>('');
   const { selectedValue, error, handleValidation, onChange } = useFormRadio({
     radioGroupName: 'Forma de pagamento',
   });
+  const [hasOrderItems, setHasOrderItems] = useState<boolean>(true);
+  const { request, isLoading, errors } = useFetch<OrderCheckout>();
   const navigate = useNavigate();
 
   const formFields = {
@@ -33,23 +47,6 @@ export default function useOrderForm(): UseOrderFormReturn {
       validators: [{ required: true }, { phone: true }],
       fieldName: 'Telefone',
       mask: 'phone',
-    }),
-    username: useFormInput({
-      validators: [{ required: true }, { minlength: 3 }],
-      fieldName: 'Nome de usuário',
-    }),
-    email: useFormInput({
-      validators: [{ required: true }, { email: true }],
-      fieldName: 'E-mail',
-    }),
-    password: useFormInput({
-      validators: [{ required: true }, { minlength: 4 }],
-      fieldName: 'Senha',
-    }),
-    passwordConfirm: useFormInput({
-      validators: [{ required: true }, { minlength: 4 }, { equals: password }],
-      fieldName: 'Confirmar senha',
-      customMessage: 'Senhas não conferem',
     }),
     cep: useFormInput({
       validators: [{ required: true }, { minlength: 9 }],
@@ -76,12 +73,118 @@ export default function useOrderForm(): UseOrderFormReturn {
     uf: useFormInput({
       validators: [{ required: true }],
       fieldName: 'UF',
+      mask: 'uf',
     }),
   };
 
-  useEffect(() => {
-    setPassword(formFields.password.value);
-  }, [formFields.password.value]);
+  function sanitizesWithSpecificity({
+    formFieldKey,
+    formFieldValue,
+  }: SanitizesWithSpecificityParams) {
+    let value = formFieldValue;
+
+    if (formFieldKey === 'phone') {
+      value = sanitizePhone(value);
+    }
+
+    return sanitizeField(value);
+  }
+
+  function formDataGrouping({
+    formFieldsObject,
+  }: FormDataGroupingParams): FormDataGroupingReturn {
+    const personalDataGroup = ['cpf', 'fullName', 'phone'];
+    const addressDataGroup = [
+      'cep',
+      'publicPlace',
+      'number',
+      'district',
+      'complement',
+      'city',
+      'uf',
+    ];
+    const personalData: { [key: string]: string } = {};
+    const addressData: { [key: string]: string } = {};
+
+    Object.entries(formFieldsObject).forEach(
+      ([formFieldKey, formFieldValue]) => {
+        if (
+          personalDataGroup.find(
+            (personalDataKey) => personalDataKey === formFieldKey
+          )
+        ) {
+          personalData[formFieldKey] = sanitizesWithSpecificity({
+            formFieldKey,
+            formFieldValue,
+          });
+        }
+
+        if (
+          addressDataGroup.find(
+            (addressDataKey) => addressDataKey === formFieldKey
+          )
+        ) {
+          addressData[formFieldKey] = sanitizesWithSpecificity({
+            formFieldKey,
+            formFieldValue,
+          });
+        }
+      }
+    );
+
+    return {
+      personalData,
+      addressData,
+    };
+  }
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setHasOrderItems(orderItems.length > 0 ?? false);
+
+    const { isValidForm, formFieldsObject } = handleSubmit({ formFields });
+    const isSelectedPaymentMethod = handleValidation();
+    const isValidOrderCheckout =
+      isAuthenticatedUser &&
+      isValidForm &&
+      hasOrderItems &&
+      isSelectedPaymentMethod;
+
+    if (isValidOrderCheckout) {
+      const { personalData, addressData } = formDataGrouping({
+        formFieldsObject,
+      });
+      const orderItemnsIdQunatity = orderItems.map(({ id, quantity }) => {
+        return {
+          id,
+          quantity,
+        };
+      });
+      const orderCheckoutData: OrderCheckout = {
+        personalData,
+        addressData,
+        paymentMethod: selectedValue.replace('radio-option-', ''),
+        orderItems: orderItemnsIdQunatity,
+      };
+      const orderCheckoutService = new OrderCheckoutService();
+
+      const response = await request(
+        orderCheckoutService.create(orderCheckoutData)
+      );
+
+      handleResponse({
+        response,
+        formFields,
+        messageTitle: 'Pedido',
+      });
+
+      if (!errors.length) {
+        clearOrderItems(false);
+        navigate('/');
+      }
+    }
+  }
 
   useEffect(() => {
     if (!isAuthenticatedUser) {
@@ -89,26 +192,15 @@ export default function useOrderForm(): UseOrderFormReturn {
     }
   }, [isAuthenticatedUser]);
 
-  function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const { isValidForm, formFieldsObject } = handleSubmit({ formFields });
-    const hasOrderItems = orderItems.length > 0;
-    const isSelectedPaymentMethod = handleValidation();
-    const isValidCheckoutOrder =
-      isAuthenticatedUser &&
-      isValidForm &&
-      hasOrderItems &&
-      isSelectedPaymentMethod;
-
-    if (isValidCheckoutOrder) {
-      console.log('Form válido');
-    }
-  }
+  useEffect(() => {
+    setHasOrderItems(orderItems.length > 0 ?? false);
+  }, [orderItems]);
 
   return {
     formFields,
+    hasOrderItems,
     paymentMethodRadio: { selectedValue, error, onChange },
+    isLoading,
     onSubmit,
   };
 }
